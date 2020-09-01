1013c9156da6c06863178fb3b7961e7b048e93dd
Imran Ali Yousifi <iyousifi@gmail.com>
2020-09-01 21:48:21 +0200

    Add Tags support

--- QuizGameBlazor/Models/Answer.cs
+++ QuizGameBlazor/Models/Answer.cs
@@ -7,6 +7,10 @@ namespace QuizGameBlazor.Models
 {
     public class Answer
     {
+        public Answer()
+        {
+            Tags = new List<AnswerTags>();
+        }
         public int Id { get; set; }
         public string Text { get; set; }
         public List<AnswerTags> Tags { get; set; }
